bca030a1e0a14a4a9f553c72850e4befafda4ced
Denis Jajčević <djajcevic@croz.net>
2024-09-24 12:42:07 +0200

    setup otel for react apps

--- react-template/%{{cookiecutter.project_slug}}%/src/index.js
+++ react-template/%{{cookiecutter.project_slug}}%/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals, {initFaroOTEL} from './reportWebVitals';
 
+initFaroOTEL()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- react-template/%{{cookiecutter.project_slug}}%/src/reportWebVitals.js
+++ react-template/%{{cookiecutter.project_slug}}%/src/reportWebVitals.js
@@ -1,3 +1,6 @@
+import {ConsoleInstrumentation, getWebInstrumentations, initializeFaro} from '@grafana/faro-web-sdk';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
@@ -10,4 +13,21 @@ const reportWebVitals = onPerfEntry => {
   }
 };
 
+var _faro = null;
+export function initFaroOTEL() {
+  _faro = initializeFaro({
+    url: `http://${process.env.REACT_APP_FARO_URL}`,
+    apiKey: `${process.env.REACT_APP_FARO_APIKEY}`,
+    trackWebVitalsAttribution: true,
+    instrumentations: [...getWebInstrumentations(), new TracingInstrumentation(), new ConsoleInstrumentation()],
+    app: {
+      name: `%{{cookiecutter.project_slug}}%`,
+      version: '1.0.0',
+    },
+  });
+}
+
+export const faro = _faro;
+
 export default reportWebVitals;
+
